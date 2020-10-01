141208c3520d395675d20ce9afc1f6a04a63a0ed
Haggai Eran <haggai.eran@gmail.com>
2020-10-01 16:14:50 +0300

    collect measurements in memory and than write to csv
    closes #1

--- ethtool-stats/main.go
+++ ethtool-stats/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"strings"
 	"time"
 
@@ -27,6 +29,7 @@ func main() {
 	interval := flag.Float64("interval", 1, "time between measurements")
 	verbose := flag.Bool("v", false, "detailed output")
 	enable_statsd := flag.Bool("statsd", false, "enable statsd reporting")
+	num_measurements := flag.Uint("measurements", 0, "number of measurements to collect")
 	usage := flag.Bool("h", false, "usage information")
 
 	var stat_names statsList
@@ -52,6 +55,11 @@ func main() {
 			"tx_vport_unicast_bytes",
 		}
         }
+
+	measurements := make([][]uint64, *num_measurements)
+	for i := range measurements {
+		measurements[i] = make([]uint64, len(stat_names) + 1)
+	}
 
 	ethHandle, err := ethtool.NewEthtool()
 	if err != nil {
@@ -79,12 +87,17 @@ func main() {
 		panic(err.Error())
 	}
 
-	start := time.Now()
+	var cur_measurement uint = 0
 
 	for now := range ticker.C {
+		now_ns := now.UnixNano()
+		if *num_measurements > 0 {
+			measurements[cur_measurement][0] = uint64(now_ns)
+		}
+
 		if *verbose {
 			fmt.Printf("%9d ns, %9d ns since last\n",
-				   now.Sub(start).Nanoseconds(),
+				   now_ns,
 				   now.Sub(prev_time).Nanoseconds())
 		}
 		stats, err := ethHandle.Stats(*netdev)
@@ -92,16 +105,48 @@ func main() {
 			panic(err.Error())
 		}
 
-		for _, stat_name := range stat_names {
+		for i, stat_name := range stat_names {
 			if *verbose {
 				fmt.Printf("%s: %d\n", stat_name, stats[stat_name]-prev[stat_name])
 			}
 			if *enable_statsd {
 				err = c.Count(stat_name, int64(stats[stat_name])-int64(prev[stat_name]), nil, 1)
 			}
+			if *num_measurements > 0 {
+				measurements[cur_measurement][i + 1] = stats[stat_name]
+			}
 		}
 
 		prev = stats
 		prev_time = now
+		cur_measurement += 1
+		if cur_measurement >= *num_measurements && *num_measurements > 0 {
+			break
+		}
+	}
+
+	if *num_measurements > 0 {
+		headers := append([]string{"time"}, []string(stat_names)...)
+
+		file, err := os.Create("result.csv")
+		if err != nil {
+			log.Fatal("Cannot create file", err)
+		}
+		defer file.Close()
+
+		writer := csv.NewWriter(file)
+		defer writer.Flush()
+
+		writer.Write(headers)
+		for _, row := range measurements {
+			row_str := make([]string, len(headers))
+			for col, val := range row {
+				row_str[col] = fmt.Sprintf("%d", val)
+			}
+			err := writer.Write(row_str)
+			if err != nil {
+				log.Fatal("Cannot write row", err)
+			}
+		}
 	}
 }
